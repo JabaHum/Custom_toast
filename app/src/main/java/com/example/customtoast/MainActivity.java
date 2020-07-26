7f34ec534117ccd3c5da05d7d06d07e77aa16f3a
narendrapatel <mitali9721@gmail.com>
2020-07-26 12:13:30 +0530

    Custom Toast

--- app/src/main/java/com/example/customtoast/MainActivity.java
+++ app/src/main/java/com/example/customtoast/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         binding.showToastButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                showToast("In Android");
+                showToast("Your Alarm is set");
             }
         });
     }
