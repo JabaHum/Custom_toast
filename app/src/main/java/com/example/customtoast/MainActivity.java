17abfda502e439c9077e6a9d6a26f41bcaf22789
narendrapatel <mitali9721@gmail.com>
2020-07-26 10:19:18 +0530

    Custom Toast

---
+++ app/src/main/java/com/example/customtoast/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.customtoast;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.customtoast.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        binding.showToastButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showToast("In Android");
+            }
+        });
+    }
+
+    private void showToast(String string) {
+
+        Context context = MainActivity.this;
+        CharSequence message = string;
+        int duration = Toast.LENGTH_SHORT;
+
+        View layout = getLayoutInflater().inflate(R.layout.custom_toast, null);
+        TextView text = layout.findViewById(R.id.textview);
+        text.setText(message);
+
+        Toast toast = new Toast(context);
+        toast.setView(layout);
+        toast.setDuration(duration);
+        toast.show();
+    }
+
+}
